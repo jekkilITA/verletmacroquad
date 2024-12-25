c900f51b5294ea089b5d1aca3ed95668e5033621
Giacomo Scavone <jekkil@proton.me>
2024-12-25 13:07:10 +0100

    constraints

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
 use macroquad::prelude::*;
 use macroquad::rand;
 
-const PARTICLE_RADIUS:f32 = 15.0;
-const CONSTRAINT_POSITION:Vec2 = Vec2::new(400.0,300.0);
+const PARTICLE_RADIUS:f32 = 3.0;
 const CONSTRAINT_RADIUS:f32 = 300.0;
-const PARTICLES_NUMBER: u32 = 10;//max 1500
+const PARTICLES_NUMBER: u32 = 400;//max 250
+const STEPS_FOR_FRAME: u8 = 8;
+
+fn hex_to_int(hex_str: &str) -> u32 {
+    u32::from_str_radix(hex_str.trim_start_matches("0x"), 16).unwrap()
+}
+
+struct GameState {
+    screen_dimensions: Vec2,
+    world: World,
+}
+
 
 
 struct Particle {
     current_position: Vec2,
     old_position: Vec2,
     acceleration: Vec2,
+     color: u32,
 }
 
 struct World {
     particles: Vec<Particle>,
 }
-
+impl GameState {
+    fn update(&mut self, delta_time: f32){
+	const GRAVITY: Vec2 = Vec2::new(0.0,1000.0);
+        for particle in &mut self.world.particles {
+            particle.update(GRAVITY,delta_time);
+	    particle.apply_constraint(self.screen_dimensions/2.0);
+	}
+        self.world.solve_collisions();
+    }
+}
 impl Particle {
-    fn new(position: Vec2) -> Self {
+    fn new(position: Vec2,color: u32) -> Self {
         Self {
             current_position: position,
             old_position: position,
             acceleration: Vec2::ZERO,
+	    color: color,
         }
     }
 
@@ -41,14 +62,14 @@ impl Particle {
     {
         self.accelerate(gravity);
     }
-    fn apply_constraint(&mut self,) {
-        let to_particle = self.current_position-CONSTRAINT_POSITION;
+    fn apply_constraint(&mut self,constra_pos:Vec2) {
+        let to_particle = self.current_position-constra_pos;
         let dist:f32 = to_particle.length();
 	// Calculate the threshold for the maximum allowed distance
 	
         if dist>(CONSTRAINT_RADIUS-PARTICLE_RADIUS) {
             let n = to_particle/dist;
-            self.current_position = CONSTRAINT_POSITION + -n*(PARTICLE_RADIUS-CONSTRAINT_RADIUS);
+            self.current_position = constra_pos + -n*(PARTICLE_RADIUS-CONSTRAINT_RADIUS);
 	}
     }
 
@@ -66,24 +87,16 @@ impl Particle {
 
 
 impl World {
-    fn update(&mut self, delta_time: f32) {
-        const GRAVITY: Vec2 = Vec2::new(0.0,1000.0);
-        for particle in &mut self.particles {
-            particle.update(GRAVITY,delta_time);
-	    particle.apply_constraint();
-	}
-        self.solve_collisions();
-    }
     fn fill(&mut self, particlesnumber: usize) {
         while self.particles.len() < particlesnumber {
-        let new_particle = Particle::new(Vec2::new(rand::gen_range(200.0,600.0), rand::gen_range(200.0,600.0)));
+        let new_particle = Particle::new(Vec2::new(rand::gen_range(200.0,600.0), rand::gen_range(200.0,600.0)),hex_to_int("2337c6"));
         self.particles.push(new_particle);
 
     }
     }
     fn render(&self) {
         for particle in &self.particles {
-            draw_circle(particle.current_position.x,particle.current_position.y, PARTICLE_RADIUS, BLUE);
+            draw_circle(particle.current_position.x,particle.current_position.y, PARTICLE_RADIUS, Color::from_hex(particle.color));
         }
     }
     fn solve_collisions(&mut self) {
@@ -103,21 +116,28 @@ impl World {
 
 #[macroquad::main("Verlet")]
 async fn main() {
-    let mut world = World {
+    let world = World {
         particles: Vec::new(),
     };
-
-    world.fill(PARTICLES_NUMBER.try_into().unwrap());
+    let mut game = GameState {
+	screen_dimensions: Vec2::new(800.0,600.0),
+	world: world,
+    };
+    game.world.fill(PARTICLES_NUMBER.try_into().unwrap());
+    set_fullscreen(true);
     let mut delta_time: f32;
     let mut fps:f32;
     loop {
+	game.screen_dimensions = Vec2::new(screen_width(),screen_height());
+	let constraint_position:Vec2 = game.screen_dimensions/2.0;
 	delta_time = get_frame_time();
         fps = (10.0/delta_time).round()/10.0;
 	let frame_time = get_frame_time()*1000.0;
-        draw_poly(CONSTRAINT_POSITION.x,CONSTRAINT_POSITION.y,96,CONSTRAINT_RADIUS,0.0,WHITE);
-	world.update(delta_time);
-
-	world.render();
+        draw_poly(constraint_position.x,constraint_position.y,96,CONSTRAINT_RADIUS,0.0,WHITE);
+	for _step in 0..STEPS_FOR_FRAME {
+	game.update(delta_time*(1.0/STEPS_FOR_FRAME as f32));
+	}
+	game.world.render();
         draw_text(&mut fps.to_string(), 700.0, 20.0, 30.0, DARKGRAY);
 	draw_text(&mut frame_time.to_string(), 700.0, 50.0, 30.0, DARKGRAY);
 	next_frame().await;
